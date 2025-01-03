3dbf970875a313b7fc0e741b2b7ed46c0155da30
hessdale <hessdale96@gmail.com>
2025-01-02 21:00:38 -0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    devtool: 'source-map',
+  }
+})
